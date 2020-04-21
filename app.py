4b1b26df3948f266a2dd94db281589e3f5f25d29
unknown <chyuting@umich.edu>
2020-04-20 22:45:49 -0400

    detailed page show accumulated/death in one figure

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from flask_caching import Cache
 import plotly
 import plotly.graph_objs as go
 import os
@@ -9,8 +10,12 @@ import sqlite3
 import crawlCDC
 import JHU_API
 
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "null", # Flask-Caching related configs
+}
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = datetime.timedelta(seconds=60)
+app.config.from_mapping(config)
 today = crawlCDC.today
 cache = crawlCDC.open_cache()
 if cache == {}:
@@ -78,6 +83,7 @@ def plot_pie_charts(cache):
     pie_fig = go.Figure(
         data=[go.Pie(labels=labels, values=values)]
     )
+    pie_fig.update_layout(uniformtext_minsize=10, uniformtext_mode='hide')
     pie_fig.write_image('static/age.png')
 
     # distribution by race
@@ -87,8 +93,22 @@ def plot_pie_charts(cache):
     pie_fig = go.Figure(
         data=[go.Pie(labels=labels, values=values)]
     )
-    pie_fig.update_layout(uniformtext_mode='hide')
+    pie_fig.update_layout(uniformtext_minsize=10, uniformtext_mode='hide')
     pie_fig.write_image('static/race.png')
+
+def plot(state_nm, data):
+    x_vals = [i[2] for i in data] # date
+    y1_vals = [i[3] for i in data] # total cases
+    y2_vals = [i[5] for i in data] # total deaths
+    fig = go.Figure(
+        data=[
+            go.Scatter(name='Accumulated cases', x=x_vals, y=y1_vals, line = dict(width=4, dash='dash')),
+            go.Scatter(name='Deaths', x=x_vals, y=y2_vals, line=dict(width=4, dash='dash'))
+        ]
+    )
+    print('generating pictures')
+    fig.update_layout(xaxis={'categoryorder': 'category ascending'})
+    fig.write_image(f'static/{state_nm}1.png')
 
 
 @app.route('/')
@@ -100,19 +120,6 @@ def index():
     state_dict = cache['state']
     return render_template('index.html', month=m, year=y, day=d, 
     cases=total_cases, deaths=total_deaths, states = state_dict)
-
-def plot(state_nm, data):
-    x_vals = [i[2] for i in data] # date
-    y_vals = [i[3] for i in data] # total cases
-    bars_data = go.Scatter(
-        x=x_vals,
-        y=y_vals,
-        line = dict(width=4, dash='dash')
-    )
-    print('generating pictures')
-    fig = go.Figure(data=bars_data)
-    fig.write_image(f'static/{state_nm}.png')
-
 
 @app.route('/state/<state_nm>')
 def state(state_nm):
@@ -128,4 +135,4 @@ if __name__ == '__main__':
     plot_stacked_bar(cache)
     plot_pie_charts(cache)
     print('starting Flask app', app.name)
-    app.run(debug=True)
+    app.run()
